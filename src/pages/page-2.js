aae3189274fa120f88f13f30e7c5170b22d63b23
Fanis Mahmalat <fanismahmalat@gmail.com>
2020-05-06 12:24:52 +0300

    Initial commit

---
+++ src/pages/page-2.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Page2 = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+    <p>Youasdasdsad.</p>
+  </Layout>
+)
+
+export default Page2
